63ad2cf19f2230e7d38c4ba179144a5bf2f1fc53
Pavlo <bazhenowp@gmail.com>
2021-01-04 16:03:47 +0200

    OTA Update added

--- ESP8266_TStat/ESP8266_TStat.ino
+++ ESP8266_TStat/ESP8266_TStat.ino
@@ -20,6 +20,9 @@
 
 #include <DNSServer.h>
 
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
+
 #include "Secrets.h"
 
 // Pinouts configuration
@@ -52,8 +55,10 @@ DeviceAddress tempDeviceAddress;
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 /* Soft AP network parameters */
-IPAddress apIP(8, 8, 8, 8);
+IPAddress apIP(192, 168, 111, 111);
+//IPAddress apIP(8, 8, 8, 8);
 IPAddress netMsk(255, 255, 255, 0);
+
 
 const byte DNS_PORT = 53;
 DNSServer dnsServer;
@@ -517,6 +522,57 @@ void setup(){
 
 
   loadParameters(); // Load parameters from EEPROM
+
+
+  // OTA firmware update *************************************
+
+
+  // Port defaults
+  ArduinoOTA.setPort(8266);		// Use 8266 port if you are working in Sloeber IDE, it is fixed there and not adjustable
+  //ArduinoOTA.setPort(3232); 	// for ArduinoIDE
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(own_ssid);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+
 }
 
 void loop(){
@@ -565,6 +621,7 @@ void loop(){
 	rescale++;
 	delay(100);
 	dnsServer.processNextRequest();
+	ArduinoOTA.handle();
 	yield();
 
 }
